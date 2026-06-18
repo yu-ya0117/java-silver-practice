f4a25d7fb89b7160fe8f65a9593f0c2a4be3625e
yu-ya <talesoftoho@gmail.com>
2026-06-18 22:31:56 +0900

    add: add chapter00 notes and source code

---
+++ chapter00/code00-03/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("1 + 2の計算を行います");
+        System.out.println("1 + 2 = " + (1 + 2));
+        System.out.println("変数を使います");
+        int num = 5;
+        System.out.println("num = " + num);
+    }
+}
