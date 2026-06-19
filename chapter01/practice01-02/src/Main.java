c926275ef4bdc6cebb90585b8036f7d255b381f1
yu-ya <talesoftoho@gmail.com>
2026-06-19 22:31:58 +0900

    add: add chapter01 notes and practice code

---
+++ chapter01/practice01-02/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println(
+            "縦幅" + a +
+            "横幅" + b +
+            "の長方形の面積は、" + c
+        );
+    }
+}
