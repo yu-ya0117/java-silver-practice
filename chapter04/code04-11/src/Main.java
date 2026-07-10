ed10e34c1023382063995bcd4462fe7eeb890d97
yu-ya <talesoftoho@gmail.com>
2026-07-10 09:02:42 +0900

    feat: add chapter04 GC and null examples

---
+++ chapter04/code04-11/src/Main.java
@@ -0,0 +1,8 @@
+public  class Main {
+    public static void main(String[] args) {
+        boolean judge = true;
+        if (judge) {
+            int[] array = {1, 2, 3};  // ブロック内で配列を作成して終了
+        }
+    }
+}
