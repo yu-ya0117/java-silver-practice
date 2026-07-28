a325fcb7d4e0a216cfc60b706ee0fb64ff89dca5
yu-ya <talesoftoho@gmail.com>
2026-07-28 10:11:51 +0900

    feat: add chapter06 package examples

---
+++ chapter06/code06-04/src/calcapp/logics/CalcLogic.java
@@ -0,0 +1,11 @@
+package calcapp.logics;
+
+public class CalcLogic {
+    public static int plus(int a, int b) {
+        return (a + b);
+    }
+
+    public static int minus(int a, int b) {
+        return (a - b);
+    }
+}
